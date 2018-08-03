cb318e45695e253dd93c7d07cf6d8f9a9ce46411
xuancao <xuancaocom@sina.cn>
2018-08-03 18:39:59 +0800

    修改

--- app/src/main/java/com/virtual/xuancao/virtualapkprogram/Helper/DownPluginHelper.java
+++ app/src/main/java/com/virtual/xuancao/virtualapkprogram/Helper/DownPluginHelper.java
@@ -6,7 +6,6 @@ import com.xuancao.base.Utils.LogUtil;
 import com.xuancao.base.Utils.ToastUtil;
 import com.xuancao.network.ProgressListener;
 import com.xuancao.network.XuancaoHttpClient;
-import com.xuancao.network.XuancaoHttpClient;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -27,27 +26,30 @@ public class DownPluginHelper {
             @Override
             public void run() {
                 XuancaoHttpClient.download(url, new Callback() {
+
                     @Override
                     public void onFailure(Call call, IOException e) {
                         ToastUtil.show(e.getMessage());
                     }
 
                     @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-
-                        long length = response.body().contentLength();
-                        InputStream is = response.body().byteStream();
-                        File file = FileUtils.getPluginCacheDir();
-                        LogUtil.e("dd",file.getAbsolutePath());
-                        String apkName = file.getAbsolutePath() + File.separator + PluginConstant.PLUGIN_NAME_REMOTE;
-                        File apkFile = new File(apkName);
-                        FileOutputStream fos = new FileOutputStream(apkFile);
-                        byte buf[] = new byte[1024];
-                        do {
-                            int numRead = is.read(buf);
-                            fos.write(buf, 0, numRead);
-                            // 当点击取消时，则停止下载
-                        } while (!isInterceptDownload);
+                    public void onResponse(Call call, Response response) {
+                        try {
+                            InputStream is = response.body().byteStream();
+                            File file = FileUtils.getPluginCacheDir();
+                            LogUtil.e("dd",file.getAbsolutePath());
+                            String apkName = file.getAbsolutePath() + File.separator + PluginConstant.PLUGIN_NAME_REMOTE;
+                            File apkFile = new File(apkName);
+                            FileOutputStream fos = new FileOutputStream(apkFile);
+                            byte buf[] = new byte[1024];
+                            do {
+                                int numRead = is.read(buf);
+                                fos.write(buf, 0, numRead);
+                                // 当点击取消时，则停止下载
+                            } while (!isInterceptDownload);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new ProgressListener() {
                     @Override
